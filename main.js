1704530adeaf1e6af21840a30446b45b96fd2610
f0m3 <mohr.nathan@googlemail.com>
2017-02-01 21:18:22 +0100

    bla

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
 /* Hello, World! program in node.js */
-console.log("Hello, World!")
+var http = require('http');
 
+var server = http.createServer(function(req, res) {
+  res.writeHead(200);
+  res.end('Hello Http');
+});
+server.listen(8080);
