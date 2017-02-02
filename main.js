be7e4cb36df0cf0c390a3bdfa432d8c1e386826e
f0m3 <mohr.nathan@googlemail.com>
2017-02-02 19:58:41 +0100

    do

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
-/* Hello, World! program in node.js */
-var http = require('http');
+var http = require("http");
 
-var server = http.createServer(function(req, res) {
-  res.writeHead(200);
-  res.end('Hello Http');
-});
-server.listen(8080);
+http.createServer(function (request, response) {
+
+   // Send the HTTP header 
+   // HTTP Status: 200 : OK
+   // Content Type: text/plain
+   response.writeHead(200, {'Content-Type': 'text/plain'});
+   
+   // Send the response body as "Hello World"
+   response.end('Hello World\n');
+}).listen(8081);
+
+// Console will print the message
+console.log('Server running at http://127.0.0.1:8081/');
